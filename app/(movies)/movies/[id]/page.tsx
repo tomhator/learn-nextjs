c35c86db194da5a1b439f1f3d668b5299cc521ce
tomhator <theoe0328@naver.com>
2025-07-21 17:22:33 +0900

    pus5

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -4,16 +4,12 @@ import MovieVideos from "../../../../components/movie-videos";
 import MovieProviders from "../../../../components/movie-providers";
 import { API_URL } from "../../../constant";
 
-interface IParams{
-    params: {id: Promise<string>};
-}
-
 async function getMovie(id: string){
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
 
-export async function generateMetadata({params: {id}}: IParams){
+export async function generateMetadata({params: {id}}: {params: {id: string}}){
     const movie = await getMovie(id);
     return {
         title: movie.title
@@ -21,20 +17,21 @@ export async function generateMetadata({params: {id}}: IParams){
 }
 
 export default async function MoviePage({
-    params
-}:IParams){
-    // 15버전 이후로 바뀐 부분. param을 await로 바꿔줘야함. 위의 Promise 함꼐 참고
-    const { id } = await params;
-
-    return <div>
-        <Suspense fallback={<h1>Loading Movie Info</h1>}>
-            <MovieInfos id={id}/>
-        </Suspense>
-        <Suspense fallback={<h1>Loading Movie Video</h1>}>
-            <MovieVideos id={id}/>
-        </Suspense>
-        <Suspense fallback={<h1>Loading Movie Porvider</h1>}>
-            <MovieProviders id={id}/>
-        </Suspense>
-    </div>
+    params: { id },
+}: {
+    params: { id: string };
+}) {
+    return (
+        <div>
+            <Suspense fallback={<h1>Loading Movie Info</h1>}>
+                <MovieInfos id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading Movie Video</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading Movie Porvider</h1>}>
+                <MovieProviders id={id} />
+            </Suspense>
+        </div>
+    );
 }
